2d9f3dd6008d38b384cc813740e9f68cbbc63c58
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2011-10-18 11:00:47 +0200

    Modified copyright notice.

--- src/java_erlang_install.erl
+++ src/java_erlang_install.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2009, Lars-Ake Fredlund
+%% Copyright (c) 2011, Lars-Ake Fredlund
 %% All rights reserved.
 %%
 %% Redistribution and use in source and binary forms, with or without
