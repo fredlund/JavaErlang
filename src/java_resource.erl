4e99c4951e297d0db9e3b004ebff5b19b6dd11c7
Joseph Wayne Norton <norton@alum.mit.edu>
2015-04-20 22:02:55 -0500

    Fix enable_gc implementation

--- src/java_resource.erl
+++ src/java_resource.erl
@@ -1,20 +1,31 @@
 -module(java_resource).
 
 -export([create/1, create/2]).
--on_load(init/0).
 
--define(NIF_STUB, nif_stub_error(?LINE)).
+-ifdef(enable_gc).
+-on_load(init/0).
 
 init() ->
     PrivDir = code:priv_dir(java_erlang),
-    ok = erlang:load_nif(PrivDir++"/java_int_resource", 0).
+    ok = erlang:load_nif(PrivDir++"/java_resource", 0).
 
-create(Term) ->
-    create(Term, self()).
+-define(NIF_STUB, nif_stub_error(?LINE)).
 
 nif_stub_error(Line) ->
     erlang:nif_error({nif_not_loaded,module,?MODULE,line,Line}).
 
+create(Term) ->
+    create(Term, self()).
+
 create(_Term, _Pid) ->
     ?NIF_STUB.
 
+-else. % ifdef(enable_gc).
+
+create(_Term) ->
+    undefined.
+
+create(_Term, _Pid) ->
+    undefined.
+
+-endif. % ifdef(enable_gc).
